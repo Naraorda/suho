508e8fa5c189f31ca4aaa66e54383b84c1d2e3c6
JGCHOI <naraorda7@gmail.com>
2018-06-10 11:18:16 +0900

    dddd

--- git_study/src/index.js
+++ git_study/src/index.js
@@ -1,3 +1,7 @@
 const sum = require('./sum');
 console.log(sum(5,6)); // 11
 console.log(sum(2,3)); // 5
+const sum = require('./sum');
+console.log(sum(5,6)); // 11
+console.log(sum(2,3)); // 5
+var
